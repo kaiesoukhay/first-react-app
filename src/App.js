db418261c9cd6cd2df98a3fc4a46100125d25328
ThinkPad <ThinkPad@DESKTOP-CM5UG90>
2021-01-16 16:02:21 +0100

    counter Commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-import './App.css';
-import Produit from './components/Produit';
-import products from  './const/Const'
+import logo from "./logo.svg";
+import "./App.css";
+import "./App.css";
+import React from "react";
+import Produit from "./components/Produit";
+import products from "./const/Const";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <div  className="App-header">{
-          products.map(produit => <Produit produit ={produit} /> )
+class App extends React.Component {
+  state = {
+    counter: 0,
+  };
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.Increment(),
+      1000
+    );
+  }
+
+  Increment = () => {
+    this.setState({
+      counter : this.state.counter + 1}) 
+  };
+  Decrement = () => {
+    this.setState({
+      counter : this.state.counter - 1}) 
+  };
+  Reser = () => {
+    this.setState({
+      counter :   0})
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div style ={{ display : 'flex' , 'flex-flow': 'column' , 'margin':'20px' }} >
+          <h1>Counter : {this.state.counter}</h1>
+          <button  onClick ={this.Increment} className= "btn btn-primary">+</button>
+          <button  onClick ={this.Decrement} className= "btn btn-primary">-</button>
+          <button  onClick ={this.Reser} className= "btn btn-primary">Reset</button>
+          </div>
+          
         
-      }</div>
         
-      </header>
-    </div>
-  );
+          <div className="App-header">
+            {products.map((produit) => (
+              <Produit produit={produit} />
+            ))}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
